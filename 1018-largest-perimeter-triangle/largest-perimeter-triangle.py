3bfdba4063ea2deb16fb5a26d76cf75c7b4cc9b8
prabhatjha8 <prabhatj267@gmail.com>
2025-09-28 15:38:10 +0530

    Time: 39 ms (12.78%) | Memory: 13.5 MB (12.29%) - LeetSync

--- 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
+++ 1018-largest-perimeter-triangle/largest-perimeter-triangle.py
@@ -1,28 +1,18 @@
-class Solution:
-    def largestPerimeter(self, nums: List[int]) -> int:
-
-        # takes in sorted array and finds the largest perimeter triangle with last value included 
-        def helper_fn(arr):
-            n = len(arr)
-            if n < 3:
-                return 0
-            else:
-                ans = 0
-                if arr[-3] + arr[-2] > arr[-1]:
-                    return arr[-1] + arr[-2] + arr[-3]
-                else:
-                    return 0
-
+class Solution(object):
+    def largestPerimeter(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         nums = sorted(nums)
         n = len(nums)
         ans = 0
         j = n - 1
-
-        while j >= 0:
-            ans = max(ans, helper_fn(nums[:j+1]))
+        while j >= 2:
+            two_sides_sum = nums[j-1] + nums[j-2]
+            if two_sides_sum > nums[j]:
+                ans = max(ans, two_sides_sum + nums[j])
             j -= 1
 
         return ans
-
-
         
